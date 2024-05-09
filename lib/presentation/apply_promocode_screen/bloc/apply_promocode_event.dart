85377d874a11eb2ce0ef7bd78c6ab48732295ef2
muhammed-sinan=vadakara <muhammedsinanvadakara@gmail.com>
2024-05-09 10:47:24 +0530

    apply promocode event

--- lib/presentation/apply_promocode_screen/bloc/apply_promocode_event.dart
+++ lib/presentation/apply_promocode_screen/bloc/apply_promocode_event.dart
@@ -1,7 +1,6 @@
 part of 'apply_promocode_bloc.dart';
 
 /// Abstract class for all events that can be dispatched from the
-///Login widget.
 ///
 /// Events must be immutable and implement the [Equatable] interface.
 class ApplyPromocodeEvent extends Equatable {
@@ -9,7 +8,6 @@ class ApplyPromocodeEvent extends Equatable {
   List<Object?> get props => [];
 }
 
-/// Event that is dispatched when the Login widget is first created.
 class ApplyPromocodeInitialEvent extends ApplyPromocodeEvent {
   @override
   List<Object?> get props => [];
